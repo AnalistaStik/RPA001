a0ab61181f15fb2e1efd130c2485cd98c82f69b0
Francisco Anderson dos Reis Nojosa <fnojosa@stik.local>
2025-03-06 14:00:17 -0300

    1.0.1

--- app.py
+++ app.py
@@ -20,23 +20,6 @@ import pytesseract
 
 def iniciar_robo():
 
-    """# Configurações do logging 
-    base_dir = os.getcwd()  # Obtém o diretório de trabalho atual
-    log_dir = os.path.join(base_dir, 'logs')  # Diretório de logs relativo ao diretório de execução
-
-    # Garantir que o diretório de logs exista
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
-
-    # Configurações do logging 
-    data_hoje = datetime.now().strftime("%d-%m-%Y")
-
-    logging.basicConfig(
-        filename=os.path.join(log_dir, f"log_{data_hoje}.txt"),
-        level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(message)s",
-    )"""
-
     senha_topmanager = "123456"
     tempo_espera_max = 30
 
@@ -90,7 +73,6 @@ def iniciar_robo():
     #Função que baixa o XML
     def baixar_XML():
         try:
-            
             time.sleep(time_short)
             pyautogui.press("F8")
             time.sleep(time_short)
@@ -206,7 +188,6 @@ def iniciar_robo():
             pyautogui.click()
             time.sleep(time_short)
 
-            
             pyautogui.moveTo(679, 86)
             pyautogui.click()
             time.sleep(1)
@@ -301,7 +282,6 @@ def iniciar_robo():
 
     except Exception as e:
         logging.error(f"Erro no processo: {e}")
-
 
 def iniciar_GUI():
     pytesseract.pytesseract.tesseract_cmd = r"C://Users//fnojosa//AppData//Local//Programs//Tesseract-OCR//tesseract.exe"
@@ -401,7 +381,6 @@ def iniciar_GUI():
         except Exception as e:
             messagebox.showerror("Erro", f"Erro ao enviar e-mail: {str(e)}")
 
-
     # Função para agrupar pedidos por NF
     def agrupar_pedidos_por_nf():
         arquivos = os.listdir(pasta_pedidos)
@@ -453,24 +432,10 @@ def iniciar_GUI():
 
             # Fechar a GUI de forma segura
             try:
-                root.quit()  # Garante o fechamento adequado
-                root.destroy()  # Garantir o encerramento da janela
+                root.quit()  
+                root.destroy()
             except Exception as e:
                 print(f"Erro ao fechar a GUI: {e}")
-
-    """def abrir_pdf_com_acrobat(pdf_path):
-        try:
-            acrobat_path = r"C://Program Files//Adobe//Acrobat DC//Acrobat//Acrobat.exe"
-            
-            if not os.path.exists(acrobat_path):
-                print(f"Erro: O arquivo {acrobat_path} não foi encontrado.")
-                return
-            
-            subprocess.Popen([acrobat_path, pdf_path])
-            print(f"PDF aberto com sucesso: {pdf_path}")
-        
-        except Exception as e:
-            print(f"Erro ao abrir o PDF com Acrobat: {e}")"""
 
     def obter_valor_nf_pdf(pdf_path):
         try:
@@ -494,24 +459,7 @@ def iniciar_GUI():
         except Exception as e:
             print(f"Erro ao extrair número da NF do PDF: {e}")
             return None
-    
-    
-    # Função para o duplo clique na Listbox
-    """def on_double_click(event):
-        selecionado = listbox.get(listbox.curselection())
-        if not selecionado:
-            return
         
-        caminho_completo = os.path.join(pasta_pedidos, selecionado)
-        
-        if selecionado.endswith(".xml"):
-            selecionados["xml"] = caminho_completo
-        elif selecionado.endswith(".pdf"):
-            selecionados["pdf"] = caminho_completo
-            abrir_pdf_com_acrobat(caminho_completo)"""
-        
-        #atualizar_label_selecionados()
-
     def atualizar_lista():
         listbox.delete(0, tk.END)
         if os.path.exists(pasta_pedidos):
@@ -544,7 +492,7 @@ def iniciar_GUI():
     # Obtém o valor da Nf do XML
     def obter_valor_nf(caminho_xml):
         try:
-            print(f"Abrindo XML: {caminho_xml}")  # Adicionando impressão de depuração
+            print(f"Abrindo XML: {caminho_xml}") 
             tree = ET.parse(caminho_xml)
             root = tree.getroot()
             ns = {'ns': 'http://www.portalfiscal.inf.br/nfe'}
@@ -560,7 +508,6 @@ def iniciar_GUI():
 
     time.sleep(2)
 
-            
     # Função para verificar os valores da nota fiscal do XML E PDF
     def verificar_correspondencia():
         if not selecionados["xml"] or not selecionados["pdf"]:
@@ -596,12 +543,6 @@ def iniciar_GUI():
             except Exception as e:
                 print(f"Erro ao renomear {os.path.basename(caminho_arquivo)}: {e}")
 
-    """def anexar_pedido():
-        if selecionados["xml"] and selecionados["pdf"]:
-            messagebox.showinfo("Sucesso", "Pedidos anexados com sucesso!")
-        else:
-            messagebox.showwarning("Erro", "Selecione um XML e um PDF antes de anexar.")"""
-            
     # Função que renomeia os arquivos automaticamente
     def renomear_pedidos_automatico(pasta_pedidos):
         arquivos = os.listdir(pasta_pedidos)
@@ -662,19 +603,12 @@ def iniciar_GUI():
     button_selecionar = ctk.CTkButton(button_frame, text="Anexar Pedidos e Enviar", command=enviar_email_com_anexo, width=120)
     button_selecionar.grid(row=0, column=0, padx=5, pady=5)
 
-    """button_anexar = ctk.CTkButton(button_frame, text="Anexar Pedido", command=anexar_pedido, width=120)
-    button_anexar.grid(row=0, column=1, padx=5, pady=5)"""
-
-    """button_enviar_pedido = ctk.CTkButton(button_frame, text="Enviar Pedido", width=120, command=enviar_email_com_anexo)
-    button_enviar_pedido.grid(row=0, column=2, padx=5, pady=5)"""
-
     label_selecionados = ctk.CTkLabel(button_frame, text="XML: Nenhum\nPDF: Nenhum", justify="left", width=40)
     label_selecionados.grid(row=1, column=0, columnspan=3, pady=5)
 
     renomear_pedidos_automatico(pasta_pedidos)
     root.after(2000, verificar_pedidos_e_enviar_email)
     atualizar_lista()
-    #listbox.bind("<Double-1>", on_double_click)
     root.mainloop()
 
 def main():
